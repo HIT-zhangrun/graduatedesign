1c413a4df3e19ada3e291ade4bc7b89af575e255
zhangrun <1139135655@qq.com>
2022-04-10 23:22:12 +0800

    fft测试成功

--- code/stm32/fft_process/FFT/fft.c
+++ code/stm32/fft_process/FFT/fft.c
@@ -2,6 +2,9 @@
 
 static float32_t fft_inputbuf[FFT_LENGTH * 2];
 static float32_t fft_outputbuf[FFT_LENGTH];
+static double range_test;
+static uint32_t max_x_test;
+static double gap_test;
 
 void fft_start()
 {
@@ -23,10 +26,38 @@ void fft_start()
             fft_inputbuf[2 * i + 1] = 0;
         }
     }
-    /*
+
     arm_cfft_radix4_f32(&scfft, fft_inputbuf);
     arm_cmplx_mag_f32(fft_inputbuf, fft_outputbuf, FFT_LENGTH);
-    */
+
+    caculate_range();
 }
+
+uint32_t find_fft_max()
+{
+	float32_t max = 0;
+	uint32_t  max_x;
+	for (int32_t i = 0; i < FFT_LENGTH/2; i++)
+	{
+		if(max < fft_outputbuf[i])
+		{
+			max = fft_outputbuf[i];
+			max_x = i;
+		}
+	}
+	return max_x;
+}
+
+double caculate_range()
+{
+	uint32_t max_x = find_fft_max();
+	max_x_test = max_x;
+	gap_test = gap;
+	double range = (max_x * gap);
+	range_test = range;
+	return range;
+}
+
+
 
 
--- code/stm32/fft_process/FFT/fft.h
+++ code/stm32/fft_process/FFT/fft.h
@@ -6,7 +6,22 @@
 
 #define FFT_LENGTH 4096
 
+#define gap    ((fs / N) * T * c / (2 * B))
+
+static double chirp   = 16;
+static double ADC_N   = 600;
+static double numRX   = 1;
+static double fs      = 2.667e6;
+static double N       = FFT_LENGTH;
+static double N1      = 2048;
+static double T       = 235e-6;
+static double B       = 3.13e9;
+static double f_begin = 78e9;
+static double c       = 2.99792458e8;
 
 void fft_start();
+uint32_t find_fft_max();
+double caculate_range();
+
 
 #endif
