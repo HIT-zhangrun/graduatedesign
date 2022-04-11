59d28f7f8af279ff163825d8669ae19fd9c061ba
zhangrun <1139135655@qq.com>
2022-04-11 20:00:18 +0800

    相位差算法实现

--- code/stm32/fft_process/Core/Src/main.c
+++ code/stm32/fft_process/Core/Src/main.c
@@ -88,7 +88,7 @@ int main(void)
   MX_USB_DEVICE_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim2);
+  //HAL_TIM_Base_Start_IT(&htim2);
 
   /* USER CODE END 2 */
 
--- code/stm32/fft_process/FFT/fft.c
+++ code/stm32/fft_process/FFT/fft.c
@@ -3,12 +3,6 @@
 static float32_t fft_inputbuf[FFT_LENGTH * 2];
 static float32_t fft_outputbuf[FFT_LENGTH];
 static double range_test;
-static uint32_t max_x_test;
-static double gap_test;
-static uint32_t time_test1;
-static uint32_t time_test2;
-static uint32_t time_test3;
-static uint32_t time_test4;
 
 void fft_start()
 {
@@ -18,7 +12,7 @@ void fft_start()
 	int16_t temp_buf;
     for (int32_t i = 0; i < FFT_LENGTH; i++)
     {
-        if(i < 600)
+        if(i < ADC_N)
         {
         	temp_buf = UserRxBufferFS[2 * i]|(UserRxBufferFS[2 * i + 1] << 8);
             fft_inputbuf[2 * i]     = (float32_t)(temp_buf);
@@ -30,13 +24,10 @@ void fft_start()
             fft_inputbuf[2 * i + 1] = 0;
         }
     }
-    TIM2->CNT = 0;
+
     arm_cfft_radix4_f32(&scfft, fft_inputbuf);
-    time_test1 =  TIM2->CNT;
     arm_cmplx_mag_f32(fft_inputbuf, fft_outputbuf, FFT_LENGTH);
-    time_test2 =  TIM2->CNT;
     caculate_range();
-    time_test3 =  TIM2->CNT;
 }
 
 uint32_t find_fft_max()
@@ -57,10 +48,8 @@ uint32_t find_fft_max()
 double caculate_range()
 {
 	uint32_t max_x = find_fft_max();
-	max_x_test = max_x;
-	gap_test = gap;
 	double range = (max_x * gap);
-	range_test = range;
+
 	return range;
 }
 
